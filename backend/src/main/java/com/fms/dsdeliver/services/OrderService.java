fa49c36577826a00f41834221fd971d3930b617d
FMS0102 <felsimoes@gmail.com>
2021-01-05 23:24:49 -0300

    GET /Orders

---
+++ backend/src/main/java/com/fms/dsdeliver/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.fms.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fms.dsdeliver.dto.OrderDTO;
+import com.fms.dsdeliver.entities.Order;
+import com.fms.dsdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+
+}
